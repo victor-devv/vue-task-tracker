878bc4c11bd048fa5b5ac4ba3ad514a85e82303a
VICTOR-TVL <vheecthurikuomola@gmail.com>
2021-03-01 18:56:05 +0100

    Frontend Done

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,122 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <Header title="Task Tracker" @toggle-add-task="toggleAddTask" :showAddTask="showAddTask" />
+    <div v-show="showAddTask">
+      <AddTask @add-task="addTask" />
+    </div>
+    <Tasks :tasks="tasks" @delete-task="deleteTask" @toggle-reminder="toggleReminder" /> <!-- v-bind tasks to tasks data in script -->
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import Header from './components/Header'
+  import Tasks from './components/Tasks'
+  import AddTask from './components/AddTask'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    components: {
+      Header,
+      Tasks,
+      AddTask
+    },
+    data() {
+      return {
+        tasks: [],
+        showAddTask: false
+      }
+    },
+    methods: {
+      addTask(task) {
+        this.tasks = [...this.tasks, task]
+      },
+      deleteTask(id) {
+        this.tasks = this.tasks.filter((task) => task.id !== id)
+      },
+      toggleReminder(id) {
+        this.tasks = this.tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task)
+      },
+      toggleAddTask() {
+        this.showAddTask = !this.showAddTask
+      }
+    },
+    created() {
+      this.tasks = [
+        {
+          id: 1,
+          text: 'Doctors Appointment',
+          day: 'March 1st, 2:00pm',
+          reminder: true,
+        },
+        {
+          id: 2,
+          text: 'Meeting at School',
+          day: 'March 3rd, 1:30pm',
+          reminder: true,
+
+        },
+        {
+          id: 3,
+          text: 'Food Shopping',
+          day: 'March 3rd, 11:00am',
+          reminder: false,
+
+        }
+      ]
+    },
   }
-}
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+  @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
+
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-family: 'Poppins', sans-serif;
+  }
+
+  .container {
+    max-width: 500px;
+    margin: 30px auto;
+    overflow: auto;
+    min-height: 300px;
+    border: 1px solid steelblue;
+    padding: 30px;
+    border-radius: 5px;
+  }
+
+  .btn {
+    display: inline-block;
+    background: #000;
+    color: #fff;
+    border: none;
+    padding: 10px 20px;
+    margin: 5px;
+    border-radius: 5px;
+    cursor: pointer;
+    text-decoration: none;
+    font-size: 15px;
+    font-family: inherit;
+  }
+
+  .btn:focus {
+    outline: none;
+  }
+
+  .btn:active {
+    transform: scale(0.98);
+  }
+
+  .btn-block {
+    display: block;
+    width: 100%;
+  }
+
 </style>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,20 @@
+<template>
+    <div :key="task.id" v-for="task in tasks">
+        <Task :task="task" @delete-task="$emit('delete-task', task.id)" @toggle-reminder="$emit('toggle-reminder', task.id)" />
+    </div>
+</template>
+
+<script>
+import Task from './Task'
+
+export default {
+    name: 'Tasks',
+    props: {
+        tasks: Array
+    },
+    components: {
+        Task
+    },
+    emits: ['delete-task', 'toggle-reminder']
+}
+</script>
